2071289ae2df156b9aa03dc0ce570089f9f68def
William Cotton <williamcotton@gmail.com>
2023-05-03 14:16:37 -0500

    Fix for issue #24

---
+++ db/migrate/20230503191616_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.text :description
+      t.boolean :isPublished
+      t.datetime :publishedAt
+
+      t.timestamps
+    end
+  end
+end
