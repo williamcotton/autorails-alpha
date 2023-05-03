2f2b5576673bb653eb6c7712dcdd8a8126c44415
William Cotton <williamcotton@gmail.com>
2023-05-03 13:32:21 -0500

    Fix for issue #22

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,5 @@
 class ProfilesController < ApplicationController
 end
 def show\n  @user = User.find(params[:id])\nend
+def edit\n  @user = User.find(params[:id])\nend
+  def update\n    @user = User.find(params[:id])\n    if @user.update(user_params)\n      redirect_to @user\n    else\n      render edit\n    end\n  end\n\n  private\n    def user_params\n      params.require(:user).permit(:first_name, :last_name)\n    end
