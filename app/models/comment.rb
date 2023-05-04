9f3916d4e984b78e0124115d81d0301ba2c62606
William Cotton <williamcotton@gmail.com>
2023-05-04 09:58:39 -0500

    Comment summary method [issue #35]
    ===
    
    Inspect the comment model. Inspect the tests.
    
    Add a summary method to the comment model. The summary method should show only the first 50 or characters, rounded to the nearest complete word.
    
    Add tests for this method and ensure that they pass.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  def summary
+    body.split[0..49].join(' ').gsub(/Ww+W*/, '...')
+  end
   belongs_to :article
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'summary should return first 50 characters rounded to nearest complete word' do
+    comment = Comment.new(body: 'This is a test comment. It should be truncated to the first 50 characters.uncated to the first 50 characters.')
+    assert_equal 'This is a test comment. It should be truncated to the first 50 characters.', comment.summary
+  end
 end
