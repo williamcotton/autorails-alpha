23ef0ed8034df970eda43256c325d7f3b889aae4
William Cotton <williamcotton@gmail.com>
2023-05-17 09:52:50 -0500

    Bug: profiles controller [issue #39]
    ===
    
    The profiles controller is broken and it needs to be fixed.
    
    The methods are outside of the class.
    
    Rewrite the controller so the methods are inside the class.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,24 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
 end
-def show\n  @user = User.find(params[:id])\nend
-def edit\n  @user = User.find(params[:id])\nend
-  def update\n    @user = User.find(params[:id])\n    if @user.update(user_params)\n      redirect_to @user\n    else\n      render edit\n    end\n  end\n\n  private\n    def user_params\n      params.require(:user).permit(:first_name, :last_name)\n    end
