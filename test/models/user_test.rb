5c83eb92a75739545316a673688b5d56ec8b9270
William Cotton <williamcotton@gmail.com>
2023-05-17 13:45:25 -0500

    Bug: user model [issue #42]
    ===
    
    The user model is broken. The methods are outside of the class definition. Fix this.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'full_name should return first and last name' do
+    user = User.new(first_name: 'John', last_name: 'Doe')
+    assert_equal 'John Doe', user.full_name
+  end
 end
