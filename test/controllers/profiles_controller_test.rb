8ee56b88e3e0fe961c59581ba3e0956879874d51
William Cotton <williamcotton@gmail.com>
2023-05-17 09:23:43 -0500

    Bug: profiles controller [issue #39]
    ===
    
    The profiles controller is broken and it needs to be fixed.

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,7 +1,25 @@
-require "test_helper"
+require 'test_helper'
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should get show' do
+    get profile_url(@user)
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_profile_url(@user)
+    assert_response :success
+  end
+
+  test 'should update user' do
+    patch profile_url(@user), params: { user: { first_name: 'New', last_name: 'Name' } }
+    assert_redirected_to profile_url(@user)
+    @user.reload
+    assert_equal 'New', @user.first_name
+    assert_equal 'Name', @user.last_name
+  end
 end
